c617b2fc79700f7ea24098a5a68a836517e449ff
Unknownbitch07 <sufiyanmp07@gmail.com>
2021-04-11 15:57:17 +0000

    tissot: Bringup PalladiumOS

--- palladium_tissot.mk
+++ palladium_tissot.mk
@@ -21,12 +21,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PalladiumOS stuff.
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+
+#PALLADIUM-PROPS
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.palladiumdevice.maintainer=Unknownbitch \
+    ro.palladiumdevice.cpu=SD625 \
+    ro.palladiumdevice.display=5.5 \
+    ro.palladiumdevice.displaytype=FHD \
+    ro.palladiumdevice.battery=3080mAh \
+    ro.palladiumdevice.camera=12MP+12MP
+
+#OFFICIAL
+PALLADIUM_BUILDTYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := palladium_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -35,7 +49,6 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 
 # Build fingerprint
